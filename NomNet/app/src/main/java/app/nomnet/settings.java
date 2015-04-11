7981b01e5dcbe7a095b6997f8ba89c3b1d0d940e
sydneyliu <sydney.liu.sl@gmail.com>
2015-04-11 12:06:47 -0700

    More settings

--- NomNet/app/src/main/java/app/nomnet/settings.java
+++ NomNet/app/src/main/java/app/nomnet/settings.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class settings extends ActionBarActivity {
@@ -12,6 +14,14 @@ public class settings extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+         R.array.privacy_arrays, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
 
