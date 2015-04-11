b1fa96d08b126e282e372c639194e97c417fc215
Rebecca Wu <wurj@usc.edu>
2015-04-11 06:15:53 -0700

    Unformatted, working version of bottom bar

--- NomNet/app/src/main/java/app/nomnet/SearchActivity.java
+++ NomNet/app/src/main/java/app/nomnet/SearchActivity.java
@@ -1,32 +1,36 @@
 package app.nomnet;
 
-import android.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
-public class FoodFeedActivity extends ActionBarActivity {
+public class SearchActivity extends ActionBarActivity {
 
     private Toolbar topbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_feed);
+        setContentView(R.layout.activity_search);
 
-       topbar = (Toolbar) findViewById(R.id.topbar);
-       setSupportActionBar(topbar);
+        topbar = (Toolbar) findViewById(R.id.topbar);
+        setSupportActionBar(topbar);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        MenuInflater inflater = getMenuInflater();
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_food_feed, menu);
-        return true;
+        inflater.inflate(R.menu.menu_search, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
