0f823fff324017234a31a7db331b883f93a5e3ae
sydneyliu <sydney.liu.sl@gmail.com>
2015-04-11 12:05:58 -0700

    Update files for settings and background

--- NomNet/app/src/main/java/app/nomnet/FoodFeedActivity.java
+++ NomNet/app/src/main/java/app/nomnet/FoodFeedActivity.java
@@ -1,6 +1,7 @@
 package app.nomnet;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -31,16 +32,14 @@ public class FoodFeedActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent mainIntent = new Intent(FoodFeedActivity.this,settings.class);
+                FoodFeedActivity.this.startActivity(mainIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
