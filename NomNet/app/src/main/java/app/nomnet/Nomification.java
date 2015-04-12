1e8c7832d0fae74ee7c2d169a1daa421c3e62359
Elliscope Fang <mingzhef@usc.edu>
2015-04-11 20:20:51 -0700

    no message

--- NomNet/app/src/main/java/app/nomnet/Nomification.java
+++ NomNet/app/src/main/java/app/nomnet/Nomification.java
@@ -1,39 +1,55 @@
 package app.nomnet;
-
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.Activity;
+
+import app.nomnet.R;
+
+public class Nomification extends Activity {
+    ListView list;
 
 
-public class Nomification extends ActionBarActivity {
+
+//hard coded part
+//override with dynamic input
+//passed in username array and imagearry
+
+    String[] web= {
+            "Sydney Liu commented on your post",
+            "Rebecca Wu commented on your post",
+            "Izzy Benavente commented on your post",
+            "Albert Yue commented on your post",
+            "Elliscope Fang commented on your post",
+    };
+
+
+    Integer[] imageId= {
+            R.drawable.albert,
+            R.drawable.albert,
+            R.drawable.albert,
+            R.drawable.albert,
+            R.drawable.albert,
+            R.drawable.albert,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nomification);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_nomification, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        CustomList adapter = new
+                CustomList(Nomification.this, web, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(Nomification.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
