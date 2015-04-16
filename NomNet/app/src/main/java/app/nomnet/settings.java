d2a2a10f5c9d10d3532dcaa6ae3f7fe947131896
Izzy Benavente <ibenaven@usc.edu>
2015-04-16 00:15:54 -0700

    Fix for register and settings files (capitalization issue)

--- NomNet/app/src/main/java/app/nomnet/settings.java
+++ NomNet/app/src/main/java/app/nomnet/settings.java
@@ -24,7 +24,7 @@ public class Settings extends ActionBarActivity {
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-         R.array.privacy_arrays, android.R.layout.simple_spinner_item);
+                R.array.privacy_arrays, android.R.layout.simple_spinner_item);
 
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
