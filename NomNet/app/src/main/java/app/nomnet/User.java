0e44cfdf529b4fdb8a34584b5525ac11f807f081
Elliscope Fang <mingzhef@usc.edu>
2015-04-23 23:17:50 -0700

    Created User Class

--- NomNet/app/src/main/java/app/nomnet/User.java
+++ NomNet/app/src/main/java/app/nomnet/User.java
@@ -1,18 +1,22 @@
 package app.nomnet;
 
+import java.util.List;
+
 /**
  * Created by Elliscope on 4/23/15.
  */
 public class User {
     private String username ;
     private String password ;
-//    private Nomification nomi;
+    private String email;
+    private Nomification nomi;
 
+    List<Nom> Nom_list;
 
-
-    public User(String username,String passowrd){
+    public User(String username,String passwordrd, String email){
         this.username = username;
-        this.password = passowrd;
+        this.password = password;
+        this.email = email;
     }
 
     public void changePassword(String new_password){
@@ -23,11 +27,21 @@ public class User {
 
     public String getPassowrd(){return password;}
 
-//    public Nomification getNomi(){return nomi;}
+    public String getEmail(){return email;}
+
+    public Nomification getNomi(){return nomi;}
 
     //function that update user' nomificaiton list
     public void NotifyUser(){
 
     }
+
+    public void addNom(Nom nom) {
+        Nom_list.add(nom);
+    }
+
+    public void encrypt(String pwd){}
+
+    public void decryp(String pwd){}
 
 }
